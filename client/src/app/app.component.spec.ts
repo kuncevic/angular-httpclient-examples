5cfd258fe5348bc268db1f0c5f32beae7bd2abee
kuncevic <kncvch@gmail.com>
2017-08-26 20:19:44 +1000

    fixup the tests, remove @angular/http refs, refactor

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -17,16 +21,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have object 'user'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.user).toBeTruthy();
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it(`should render button with text 'Post to server'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('button').textContent).toContain('Post to server');
   }));
 });
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Headers , RequestOptions } from '@angular/http';
 import { HttpHeaders } from '@angular/common/http';
 
 @Component({
@@ -9,23 +8,20 @@ import { HttpHeaders } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Test';
   user = { id : 1, name : 'Hello'};
 
   constructor(private http: HttpClient) { }
 
   callServer() {
-    const headers = new Headers({ 'Content-Type': 'application/json' });
-    const options = new RequestOptions({ headers: headers });
+    const headers = new HttpHeaders()
+          .set('Authorization', 'my-auth-token')
+          .set('Content-Type', 'application/json');
 
     this.http.post('http://127.0.0.1:3000/ping', JSON.stringify(this.user), {
-      headers: new HttpHeaders()
-        .set('Authorization', 'my-auth-token')
-        .set('Content-Type', 'application/json')
+      headers: headers
     })
     .subscribe(data => {
       console.log(data);
-      // do something here
     });
   }
 }
