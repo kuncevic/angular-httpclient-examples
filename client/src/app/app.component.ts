9047f57fb4275f2c44b3d3b468faa71a5ae3e755
kuncevic <kncvch@gmail.com>
2017-08-26 20:37:19 +1000

    organize imports

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
