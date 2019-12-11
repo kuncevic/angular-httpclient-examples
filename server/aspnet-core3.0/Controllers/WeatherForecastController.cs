026c3be9c98899babc5ab4e0fad371674d4760a6
kuncevic <kncvch@gmail.com>
2019-12-11 15:32:53 +1100

    (wip): add aspnet core 3.0

---
+++ server/aspnet-core3.0/Controllers/WeatherForecastController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    //[EnableCors("MyPolicy")]
+    //ref: https://docs.microsoft.com/en-us/aspnet/core/security/enforcing-ssl?view=aspnetcore-3.1&tabs=visual-studio
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpPost]
+        public dynamic Post([FromBody]dynamic data)
+        {
+            data.text = $"{data.text} from .NET Core";
+            return data;
+        }
+    }
+}
