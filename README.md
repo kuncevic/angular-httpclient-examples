13a077814cfae94fd62c452797b3f4f143eafb87
kuncevic <kncvch@gmail.com>
2021-03-16 18:18:08 +1100

    README.md

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 - [nodejs express server](https://github.com/kuncevic/angular-httpclient-examples/tree/master/server/express)
 - [aspnet core 2.0 server](https://github.com/kuncevic/angular-httpclient-examples/tree/master/server/aspnet-core2.0)
 - go //TODO
+
+### Want to learn more Angular? 
+🎙 [Angular Rocks Podcast](https://angularrocks.com)  
+🚀 [Angular Tips and Tricks Newsletter](https://kuncevic.dev/daily)
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
