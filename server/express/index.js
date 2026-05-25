2d847ca72d7f26728e28c4b90b37b6b3707e101e
kuncevic <kncvch+code@gmail.com>
2026-05-25 22:45:34 +1000

    update packages; use express v5

--- server/express/index.js
+++ server/express/index.js
@@ -1,11 +1,10 @@
 const express = require('express')
-const bodyParser = require('body-parser');
 const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}) );
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.all("/*", function(req, res, next){
+app.use(function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -17,6 +16,10 @@ app.post('/ping', function (req, res) {
   res.send(req.body)
 })
 
-app.listen(3000, function () {
+app.listen(3000, function (err) {
+  if (err) {
+    console.error('Failed to start server:', err.message)
+    process.exit(1)
+  }
   console.log('Nodejs app listening on port 3000')
 })
